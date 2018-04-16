6e47d41ad4405d5224c67b81e6b44ac5632f6c2b
Jaime Burbano <burbanojaime98@gmail.com>
2018-04-16 08:54:09 -0500

    Parte donde me perdí

--- index.js
+++ index.js
@@ -16,13 +16,19 @@ app.set('view engine', 'hbs');
 //Cambio la carpeta donde tengo los templates que quiero
 app.set('views', './views');
 
+//Para colocar archivos estaticos como el CSS
+//-------->el segundo 'css' es la carpeta donde tengo mis estilos
+/*-------->app.use('/css',express.static('css'));*/
+
+
+
 
 app.get('/', function (req, res) {
     //El 'index' es el template que usare que esta en la carpeta views
     res.render('index', {
         titulo: 'Hola',
-        logohbs:'https://handlebarsjs.com/images/handlebars_logo.png'
-
+        logohbs: 'https://handlebarsjs.com/images/handlebars_logo.png',
+        personas: ['Isita', 'isa', 'isota'],
     });
 });
 
