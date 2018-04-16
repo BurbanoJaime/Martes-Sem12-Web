f1d185b071a07ac9f9380aca02571a73d1e1d143
Jaime Burbano <burbanojaime98@gmail.com>
2018-04-16 08:07:09 -0500

    Configuracion del proyecto

---
+++ index.js
@@ -0,0 +1,30 @@
+//Instalo las librerias que voy a usar
+
+const express = require('express'),
+    consolidate = require('consolidate'),
+    hbs = require('handlebars');
+
+//Creo las variables de express
+var app = express();
+
+//Comunicación de express con handlebars
+app.engine('hbs', consolidate.handlebars);
+
+//Cambio la configuración del motor de templates por defecto a los handlebars
+app.set('view engine', 'hbs');
+
+//Cambio la carpeta donde tengo los templates que quiero
+app.set('views', '/views');
+
+
+app.get('/', function (req, res) {
+    //El 'index' es el template que usare que esta en la carpeta views
+    res.render('index', {
+        titulo: 'Hola'
+    });
+});
+
+
+app.listen(3000, function () {
+    console.log("Funciona");
+});
